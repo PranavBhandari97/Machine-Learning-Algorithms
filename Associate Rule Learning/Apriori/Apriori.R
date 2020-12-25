9ec971285b23539bd9e1c0ed19640b390433785a
Pranav Bhandari <pranavbhandari97@gmail.com>
2020-12-25 23:13:04 +0530

    Add files via upload

---
+++ Associate Rule Learning/Apriori/Apriori.R
@@ -0,0 +1,35 @@
+# Apriori
+
+#Setting working directory
+getwd()
+setwd('E:/Pranav/Portfolio/Github/Machine Learning/Associate Rule Learning/Apriori')
+
+#Data Preprocessing
+#install.packages('arules')
+library(arules)
+
+#Importing the dataset
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+
+#Exploring the dataset
+head(dataset)
+tail(dataset)
+nrow(dataset)
+ncol(dataset)
+str(dataset)
+summary(dataset)
+
+#Building the Sparse Matrix
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+
+#Viewing details of Sparse Matrix
+summary(dataset)
+
+#Plotting the 10 highest bought products
+itemFrequencyPlot(dataset, topN = 10)
+
+#Training Apriori on the dataset
+rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
+
+#Visualising the results
+inspect(sort(rules, by = 'lift')[1:10])
