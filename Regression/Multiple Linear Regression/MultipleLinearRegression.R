c20333fc2d9f92c4bac9e600ffbc5504d70e8fd6
Pranav Bhandari <pranavbhandari97@gmail.com>
2020-12-25 23:52:39 +0530

    Add files via upload

---
+++ Regression/Multiple Linear Regression/MultipleLinearRegression.R
@@ -0,0 +1,57 @@
+# Multiple Linear Regression
+
+#Setting the working directory
+getwd()
+setwd('E:/Pranav/Portfolio/Github/Machine Learning/Regression/Multiple Linear Regression')
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+#Exploring the dataste
+head(dataset)
+tail(dataset)
+nrow(dataset)
+ncol(dataset)
+str(dataset)
+summary(dataset)
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+#Check
+head(dataset)
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Multiple Linear Regression to the Training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+#Checking performance of Regressor
+summary(regressor)
+
+#Improving the model
+regressor = lm(formula = Profit ~(R.D.Spend+Administration+Marketing.Spend),
+               data = training_set)
+summary(regressor)
+regressor = lm(formula = Profit ~(R.D.Spend+Marketing.Spend),
+               data = training_set)
+summary(regressor)
+regressor = lm(formula = Profit ~(R.D.Spend),
+               data = training_set)
+summary(regressor)
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+
+#Viewing comparing the results
+compare = list(test_set$Profit, y_pred)
+compare
