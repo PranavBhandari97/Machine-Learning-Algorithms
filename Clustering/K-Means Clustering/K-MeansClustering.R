7454506eec6257cfa9583bc1e068d438c0f8054e
Pranav Bhandari <pranavbhandari97@gmail.com>
2020-12-25 23:30:05 +0530

    Add files via upload

---
+++ Clustering/K-Means Clustering/K-MeansClustering.R
@@ -0,0 +1,51 @@
+# K-Means Clustering
+
+#Setting working directory
+getwd()
+setwd('E:/Pranav/Portfolio/Github/Machine Learning/Clustering/K-Means Clustering')
+#Importing the dataset
+dataset = read.csv('Mall_Customers.csv')
+
+#Exploring the dataset
+head(dataset)
+tail(dataset)
+nrow(dataset)
+ncol(dataset)
+str(dataset)
+summary(dataset)
+
+#For clustering, we need only Income and Spending Score. So taking those columns.
+dataset = dataset[4:5]
+
+#Check
+head(dataset)
+
+#Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
+
+#Fitting K-Means to the dataset
+set.seed(29)
+kmeans = kmeans(x = dataset, centers = 5)
+y_kmeans = kmeans$cluster
+
+#Visualising the clusters
+library(cluster)
+clusplot(dataset,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
