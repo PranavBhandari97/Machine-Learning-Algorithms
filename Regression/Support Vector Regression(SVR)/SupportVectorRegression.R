e68f52b4dabf18cf7e9f39705467a0e54c5245b9
Pranav Bhandari <pranavbhandari97@gmail.com>
2020-12-25 23:59:33 +0530

    Add files via upload

---
+++ Regression/Support Vector Regression(SVR)/SupportVectorRegression.R
@@ -0,0 +1,44 @@
+# SVR
+
+#Setting the working directory
+getwd()
+setwd('E:/Pranav/Portfolio/Github/Machine Learning/Regression/Support Vector Regression(SVR)')
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+
+#Exploring the dataset
+head(dataset)
+tail(dataset)
+ncol(dataset)
+nrow(dataset)
+str(dataset)
+summary(dataset)
+
+#As position column is not needed we remove it
+dataset = dataset[2:3]
+
+# Fitting SVR to the dataset
+#install.packages("e1071")
+library(e1071)
+regressor = svm(Salary~Level, 
+                data = dataset, 
+                type = "eps-regression")
+
+#Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+#Check
+y_pred
+
+# Visualising the SVR results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (SVR)') +
+  xlab('Level') +
+  ylab('Salary')
